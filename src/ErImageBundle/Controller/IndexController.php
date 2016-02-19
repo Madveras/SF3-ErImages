6339eeec4548db721fea1b1ade466b4109c1f847
madveras <david@madteam.net>
2016-02-19 13:01:29 +0100

    Listado de carpetas

---
+++ src/ErImageBundle/Controller/IndexController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace ErImageBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+
+class IndexController extends Controller
+{
+    public function indexAction(Request $request)
+    {              
+        return $this->render(
+            'ErImageBundle:indexIndex.html.twig'
+            );
+    }
+}
